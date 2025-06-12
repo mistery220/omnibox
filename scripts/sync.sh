baa32d36b33547b6fd23205ad708973c9cb57010
mistery220 <basketball.coder.pro@gmail.com>
2025-06-13 07:35:37 +0800

    Update script, sync to main branch

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
+
 for dir in "web" "backend" "wizard"; do
-    cd "$dir" && git checkout main && git pull origin main
-    cd ..
+    (
+        cd "$dir" && git checkout main && git pull origin main
+    ) &
 done
 
-cd "client/browser-extension" && git checkout main && git pull origin main
-cd "../.."
+(
+    cd "client/browser-extension" && git checkout main && git pull origin main
+) &
+
+wait
